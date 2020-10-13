b36749fb5cb40844060f443ea1bdabf387356155
Roman Aleksyutin <gyxman@yandex.ru>
2020-10-13 22:50:56 +0300

    add hw

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,6 +1,6 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
-import Redirect from "react-router-dom";
+import {Redirect} from "react-router-dom";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
 import {GameType} from "../../const";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
